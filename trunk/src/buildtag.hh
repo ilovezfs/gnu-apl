4fa3a1c69ddda3d9a9328bc5dd8506ed4de8c457
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2013-11-06 12:40:12 +0000

    fixed fault in format-by-example

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,2 +1,2 @@
 #include "../config.h"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4460", "2013-11-05 12:18:11 UTC", "Linux 2.6.32-52-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4466", "2013-11-06 12:33:59 UTC", "Linux 2.6.32-52-generic i686", ""
