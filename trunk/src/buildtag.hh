9ce2a3c600ba57d1d2cb3d26bedda2c85a3c7738
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2013-11-29 13:12:31 +0000

    fixed wrong Assertion in Parser.cc

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,2 +1,2 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4624", "2013-11-28 18:33:14 UTC", "Linux 2.6.32-52-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4627", "2013-11-29 13:08:18 UTC", "Linux 2.6.32-52-generic i686", ""
