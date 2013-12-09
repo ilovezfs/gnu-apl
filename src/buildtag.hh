16b60a4b91782f46bd0beceaa55c6151121a32d0
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2013-12-09 18:55:13 +0000

    fixed 2⌹3 problem

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,2 +1,2 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4727", "2013-12-09 14:46:46 UTC", "Linux 2.6.32-54-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4732", "2013-12-09 18:40:58 UTC", "Linux 2.6.32-54-generic i686", ""
