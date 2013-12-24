f6d824e5fb2952c44c466ae9349a7e2380947a00
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2013-12-24 17:26:55 +0000

    changed ∈ to ∊ in ⎕CR

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,2 +1,2 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4938", "2013-12-24 16:21:45 UTC", "Linux 2.6.32-54-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 4942", "2013-12-24 17:23:44 UTC", "Linux 2.6.32-54-generic i686", ""
