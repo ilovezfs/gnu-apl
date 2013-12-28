2e7c83e59d87caec9391dbb50fbd9287e65161f7
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2013-12-28 15:57:37 +0000

    fixed direntry issue in debian packaging

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,2 +1,2 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 5010", "2013-12-28 15:31:18 UTC", "Linux 2.6.32-54-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 5013", "2013-12-28 15:54:12 UTC", "Linux 2.6.32-54-generic i686", ""
