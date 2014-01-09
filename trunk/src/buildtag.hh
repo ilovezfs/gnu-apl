99186023dc7497b8c0bf2feaf144e62daaa9fc02
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-01-09 18:28:22 +0000

    emacs mode added

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,2 +1,2 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 5223", "2014-01-09 17:28:26 UTC", "Linux 2.6.32-54-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 5229", "2014-01-09 18:25:48 UTC", "Linux 2.6.32-54-generic i686", ""
---
+++ trunk/src/emacs_mode/emacs.hh
@@ -0,0 +1,50 @@
+/*
+    This file is part of GNU APL, a free implementation of the
+    ISO/IEC Standard 13751, "Programming Language APL, Extended"
+
+    Copyright (C) 2014  Elias Mårtenson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef EMACS_HH
+#define EMACS_HH
+
+// #pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wpragmas"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+#pragma GCC diagnostic ignored "-Wparentheses"
+#pragma GCC diagnostic ignored "-Wreorder"
+#pragma GCC diagnostic ignored "-Wmismatched-tags"
+#pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#include "Native_interface.hh"
+#pragma GCC diagnostic pop
+
+void set_active( bool v );
+
+#define END_TAG "APL_NATIVE_END_TAG"
+
+class LockWrapper
+{
+public:
+    LockWrapper() { set_active( true ); };
+    virtual ~LockWrapper() { set_active( false ); };
+};
+
+const UCS_string ucs_string_from_string( const std::string &string );
+
+#endif
