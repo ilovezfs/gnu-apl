98a14f2566a95cd940184a8cf9fb8cf8d45e4b5d
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-05-11 12:38:54 +0000

    fixed stale ⎕NLT when )LOADing workspaces

--- src/makefile.h
+++ src/makefile.h
@@ -1,6 +1,6 @@
 // some strings exported from Makefile
 //
 #define Makefile__sysconfdir "/etc"
-#define Makefile__pkglibdir  "/usr/lib64/apl"
+#define Makefile__pkglibdir  "/usr/lib/apl"
 #define Makefile__localedir  "/usr/share/locale"
 
