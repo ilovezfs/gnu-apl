ea997a1febaa8e366a6efd2a56437878cbaaaf62
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-05-24 13:17:27 +0000

    fixed syntax error on execute of function not returning a value

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 6730", "2014-05-23 13:44:24 UTC", "Linux 2.6.32-54-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 6737", "2014-05-24 13:06:48 UTC", "Linux 2.6.32-54-generic i686", ""
 #define ARCHIVE_SVN  6693
