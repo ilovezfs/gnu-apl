8488b30b42529816574a9b2dd23a828bc5e7ebda
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-06-21 12:56:12 +0000

    fixed fault in Archive.cc

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7134", "2014-06-21 12:19:47 UTC", "Linux 2.6.32-54-generic i686", "'DEVELOP_WANTED=yes'"
-#define ARCHIVE_SVN  7112
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7136", "2014-06-21 12:51:02 UTC", "Linux 2.6.32-54-generic i686", "'--host=i486-pc-linux-gnu' '--build=i486-pc-linux-gnu' '--target=i486-rpm-linux' '--program-prefix=' '--prefix=/usr' '--exec-prefix=/usr' '--bindir=/usr/bin' '--sbindir=/usr/sbin' '--sysconfdir=/etc' '--datadir=/usr/share' '--includedir=/usr/include' '--libdir=/usr/lib' '--libexecdir=/usr/libexec' '--localstatedir=/var' '--sharedstatedir=/usr/com' '--mandir=/usr/share/man' '--infodir=/usr/share/info' 'build_alias=i486-pc-linux-gnu' 'host_alias=i486-pc-linux-gnu' 'target_alias=i486-rpm-linux' 'CFLAGS=-O2 -g -march=i486' 'CXXFLAGS=-O2 -g -march=i486'"
+#define ARCHIVE_SVN  7136
