2b893f5796edcd630ff468414e9a4b38334c23ac
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-07-08 11:59:34 +0000

    fixed preferences installation

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7389", "2014-07-08 09:58:22 UTC", "Linux 3.11.0-12-generic x86_64", "'DEVELOP_WANTED=yes'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7395", "2014-07-08 11:57:40 UTC", "Linux 3.11.0-12-generic x86_64", ""
 #define ARCHIVE_SVN  7326
