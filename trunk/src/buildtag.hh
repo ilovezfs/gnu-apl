bab9c1705b32e0ab1228eaaa1adea4d92727c09b
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-07-22 10:16:02 +0000

    updated sql and emacs subdirs

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7617", "2014-07-21 18:02:07 UTC", "Linux 3.11.0-12-generic x86_64", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7619", "2014-07-22 10:13:51 UTC", "Linux 3.11.0-12-generic x86_64", "'--host=x86_64-pc-linux-gnu' '--build=x86_64-pc-linux-gnu' '--program-prefix=' '--disable-dependency-tracking' '--prefix=/usr' '--exec-prefix=/usr' '--bindir=/usr/bin' '--sbindir=/usr/sbin' '--sysconfdir=/etc' '--datadir=/usr/share' '--includedir=/usr/include' '--libdir=/usr/lib64' '--libexecdir=/usr/lib/x86_64-linux-gnu' '--localstatedir=/var' '--sharedstatedir=/usr/com' '--mandir=/usr/share/man' '--infodir=/usr/share/info' 'build_alias=x86_64-pc-linux-gnu' 'host_alias=x86_64-pc-linux-gnu' 'CFLAGS=-O2 -g' 'CXXFLAGS=-O2 -g'"
 #define ARCHIVE_SVN  7326
