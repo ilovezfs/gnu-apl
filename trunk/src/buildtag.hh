6bf4db097c0c17008792c05ab0a4463091930492
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-08-10 09:11:03 +0000

    fixed debian i386 install problem

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7887", "2014-08-07 15:46:58 UTC", "Linux 3.11.0-12-generic x86_64", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7917", "2014-08-10 08:54:08 UTC", "Linux 3.11.0-12-generic x86_64", "'DEVELOP_WANTED=yes'"
 #define ARCHIVE_SVN  7773
