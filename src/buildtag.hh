4d480e4f0f67d39bafb3f203640037c4a03e221f
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-08-10 09:52:57 +0000

    updated WASD layout, filexd negative PC in ⎕LC

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7917", "2014-08-10 08:54:08 UTC", "Linux 3.11.0-12-generic x86_64", "'DEVELOP_WANTED=yes'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 7922", "2014-08-10 09:50:32 UTC", "Linux 3.11.0-12-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN  7773
