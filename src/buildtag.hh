e2de04bdcb2b83bff2852be3169e3d75fc255a1e
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-08-22 13:22:59 +0000

    improved ]PSTAT command

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 8047", "2014-08-22 13:10:25 UTC", "Linux 3.11.0-12-generic x86_64", "'--host=x86_64-pc-linux-gnu' '--build=x86_64-pc-linux-gnu' '--program-prefix=' '--disable-dependency-tracking' '--prefix=/usr' '--exec-prefix=/usr' '--bindir=/usr/bin' '--sbindir=/usr/sbin' '--sysconfdir=/etc' '--datadir=/usr/share' '--includedir=/usr/include' '--libdir=/usr/lib64' '--libexecdir=/usr/lib/x86_64-linux-gnu' '--localstatedir=/var' '--sharedstatedir=/usr/com' '--mandir=/usr/share/man' '--infodir=/usr/share/info' 'build_alias=x86_64-pc-linux-gnu' 'host_alias=x86_64-pc-linux-gnu' 'CFLAGS=-O2 -g' 'CXXFLAGS=-O2 -g'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 8049", "2014-08-22 13:17:47 UTC", "Linux 3.11.0-12-generic x86_64", ""
 #define ARCHIVE_SVN  7773
