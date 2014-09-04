5f18b43bf9574421ac7ad2a3991f8c40a6190e52
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-09-04 13:11:03 +0000

    user contributed packages updated

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 8186", "2014-09-03 17:56:31 UTC", "Linux 3.11-2-amd64 x86_64", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 8197", "2014-09-04 13:06:43 UTC", "Linux 3.11-2-amd64 x86_64", ""
 #define ARCHIVE_SVN  7773
