76608b23611c6cd118d2edb4b0488225aac6e222
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-02-15 10:30:26 +0000

    fixed duplicate fwrite() in FILE_IO

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9038", "2015-02-10 13:11:36 UTC", "Linux 3.13.0-37-generic i686", "'--enable-maintainer-mode' 'DEVELOP_WANTED=yes'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9041", "2015-02-15 10:28:08 UTC", "Linux 3.13.0-37-generic i686", "'--host=i686-pc-linux-gnu' '--build=i686-pc-linux-gnu' '--program-prefix=' '--disable-dependency-tracking' '--prefix=/usr' '--exec-prefix=/usr' '--bindir=/usr/bin' '--sbindir=/usr/sbin' '--sysconfdir=/etc' '--datadir=/usr/share' '--includedir=/usr/include' '--libdir=/usr/lib' '--libexecdir=/usr/lib/i386-linux-gnu' '--localstatedir=/var' '--sharedstatedir=/usr/com' '--mandir=/usr/share/man' '--infodir=/usr/share/info' 'build_alias=i686-pc-linux-gnu' 'host_alias=i686-pc-linux-gnu' 'CFLAGS=-O2 -g -march=i486' 'CXXFLAGS=-O2 -g -march=i486'"
 #define ARCHIVE_SVN  9016
