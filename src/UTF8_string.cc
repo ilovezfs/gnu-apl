9e7534c4f65fbcf14c040f8af00437e4b7f490e7
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-02-26 12:49:08 +0000

    libapl.h and libapl.cc added

--- src/UTF8_string.cc
+++ src/UTF8_string.cc
@@ -46,10 +46,10 @@
    0000 0800-0000 FFFF   1110xxxx 10xxxxxx 10xxxxxx
    0001 0000-001F FFFF   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
    0020 0000-03FF FFFF   111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
-   0400 0000-7FFF FFFF   1111110x 10xxxxxx ... 10xxxxxx
+   0400 0000-7FFF FFFF   1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
 
-   In order to be more general, we follow RFC 2279 and allow 5-byte and 6-byte
-   encodings
+   In order to be more general, we follow RFC 2279 and also allow 5-byte and
+   6-byte encodings
 */
 
 //-----------------------------------------------------------------------------
