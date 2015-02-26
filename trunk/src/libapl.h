b3c089ee725b841600bd676336d89d18a4f4999a
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-02-26 17:23:23 +0000

    libapl.txt added

--- trunk/src/libapl.h
+++ trunk/src/libapl.h
@@ -1,3 +1,24 @@
+/*
+    This file is part of GNU APL, a free implementation of the
+    ISO/IEC Standard 13751, "Programming Language APL, Extended"
+
+    Copyright (C) 2015  Dr. Dirk Laurie
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef __LIBAPL_H_DEFINED__
 #define __LIBAPL_H_DEFINED__
 
@@ -7,91 +28,9 @@
 extern "C" {
 #endif
 
-/* Application Program Interface for GNU APL
-
-The following facilities are provided:
-
-`apl_exec`
-~   pass a C string to the interpreter for immediate execution as APL code.
-`apl_command`
-~   pass an APL command to the command processor and return its output.
-`APL`
-~   a container class wrapping an APL value, allowing access to its rank, 
-    shape and ravel.
-`apl_get`
-~   construct a read-only object of class `APL` given the name of a 
-    variable in the workspace.
-`apl_set`
-~   change the value of a variable in the workspace to a value wrapped
-    by an object of class `APL`.
-
-All the action takes place in a private implementation class. No GNU APL
-header is exposed.
-
-Vague details of the GNU APL implementation
--------------------------------------------
-
-Although the implementation is hidden from the API, the programmer
-needs to know a little about it.
-
-Only one workspace, simply known as "the workspace", is active at any
-time. The workspace may be cleared, named, saved and restored by calls
-to `apl_command`.
-
-The workspace contains a collection of symbols of various kinds. Apart 
-from `apl_exec` and `apl_command`, which behave as if entered from the 
-keyboard in an interactive APL session, this API gives access only to 
-APL variables, i.e. symbols associated with values.
-
-A value is a multidimensional array of cells. It has three visible
-components: rank, shape and ravel.
-
-The shape is a vector of integers, giving the number of elements along 
-each axis of the array. Thus the number of elements in the array is
-given by the product of the shape items. The number of shape items is
-known as the rank. An empty product is of course equal to 1, so that 
-a scalar has rank 0.
-
-The ravel is a vector of cells, accessed in APL by a multi-index but in
-the interface by a single index starting at 0. As one progresses along 
-the ravel, the multi-index is ordered lexicographically, e.g. in a clear 
-workspace, the multi-index of an array of shape `2 3` would take the 
-values `1 1`, `1 2`, `1 3`, `2 1`, `2 2`, `2 3`. The index origin in APL 
-may be changed by `apl_exec`, but in the interface the ravel is always 
-indexed from 0.
-
-A cell can hold any of several kinds of objects:
-
-1. A scalar value, i.e. either a number or a single 16-bit Unicode character.
-   The number may be stored internally as a 64-bit integer, a `double`, or
-   a `complex<double>`.
-2. A smart pointer to a value. This allows nested arrays to be represented.
-3. Nothing. This can only appear in objects of type `newAPL` that have
-   not been initialized fully. 
-4. None of the above, i.e. information not accessible from the API.
-
-Class `APL`
------------
-
-The user can see rank, shape items and ravel elements. They are 
-constructed in four ways:
-
-1.  Extracting a snapshot of a variable from the workspace using 
-    `apl_get`. The ravel is extracted "by name". That is to say, 
-    changes to the elements of the ravel made by `apl_exec` will be 
-    visible in the state. 
-2.  Newly created scalars and character vectors.
-3.  The copy constructor makes a deep copy. 
-4.  Creating a new value of given shape in which all cells contain
-    nothing. The user is responsible for filling these cells before 
-    calling `apl_set`.
-
-Ravel elements can be changed except in the case of objects created 
-by `apl_get`, which returns a constant pointer. Overriding this by
-a typecast is strongly deprecated and may have unpredictable side 
-effects.
-
-*/
+/* Application Program Interface for GNU APL...
+   The details of this API are described in libapl.txt in this directory
+ */
 
 /// LOC is a macro that expands to the source filename and line number as 
 /// a C string literal. It should (but does not have to) be used as the
@@ -140,19 +79,20 @@ APL_value char_scalar(int unicode, const char * loc);
 APL_value apl_value(int rank, const int64_t * shape, const char * loc);
 
 /// a new scalar with ravel 0
-APL_value apl_scalar(const char * loc)
+inline APL_value apl_scalar(const char * loc)
    { return apl_value(0, 0, loc); }
 
-/// a new vector with a ravel of length len and ravel elements are initialized to 0
-APL_value apl_vector(int64_t len, const char * loc)
+/// a new vector with a ravel of length len and ravel elements initialized to 0
+inline APL_value apl_vector(int64_t len, const char * loc)
    { return apl_value(1, &len, loc); }
 
 /// a new matrix. all ravel elements are initialized to integer 0
-APL_value apl_maxtrix(int64_t rows, int64_t cols, const char * loc)
+inline APL_value apl_matrix(int64_t rows, int64_t cols, const char * loc)
    { const int64_t sh[] = { rows, cols };   return apl_value(2, sh, loc); }
 
 /// a new 3-dimensional value. all ravel elements are initialized to integer 0
-APL_value apl_cube(int64_t blocks, int64_t rows, int64_t cols, const char *loc)
+inline APL_value apl_cube(int64_t blocks, int64_t rows, int64_t cols,
+                          const char *loc)
    { const int64_t sh[] = { blocks, rows, cols };
      return apl_value(3, sh, loc); }
 
