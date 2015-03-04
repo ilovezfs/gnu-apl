ba42d7cf5854463ca1f073ec9b1f49e79e4455a1
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-03-04 15:12:27 +0000

    fixed installation of libapl.info

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9209", "2015-03-04 12:53:33 UTC", "Linux 3.13.0-37-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9213", "2015-03-04 15:10:32 UTC", "Linux 3.13.0-37-generic i686", ""
 #define ARCHIVE_SVN  9095
