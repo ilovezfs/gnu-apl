5771a38e92c14b35c84fafb03ebd4808170e5e38
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-03-04 13:02:04 +0000

    added libapl documentation in doc directory

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9188", "2015-03-03 13:52:04 UTC", "Linux 3.13.0-37-generic i686", "'--enable-maintainer-mode' 'DEVELOP_WANTED=yes' '--with-libapl'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9209", "2015-03-04 12:53:33 UTC", "Linux 3.13.0-37-generic i686", ""
 #define ARCHIVE_SVN  9095
