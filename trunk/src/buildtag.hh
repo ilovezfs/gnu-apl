53acf39bc7a5f92ac263f47d56ccbfbcd26a6c7e
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-03-06 17:38:19 +0000

    added AC_CONFIG_AUX_DIR in configure.ac

--- trunk/src/buildtag.hh
+++ trunk/src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9233", "2015-03-06 12:40:36 UTC", "Linux 3.13.0-37-generic i686", "'--enable-maintainer-mode' 'DEVELOP_WANTED=yes'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9237", "2015-03-06 17:28:38 UTC", "Linux 3.13.0-37-generic i686", ""
 #define ARCHIVE_SVN  9227
