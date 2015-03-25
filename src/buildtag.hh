1ea01ce42b6bfaf298e67f7e7998618a76a35269
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-03-25 14:30:47 +0000

    improved apl.info installation

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9471", "2015-03-25 13:23:08 UTC", "Linux 3.13.0-37-generic i686", "'--enable-maintainer-mode' 'DEVELOP_WANTED=yes'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9477", "2015-03-25 14:28:25 UTC", "Linux 3.13.0-37-generic i686", "'--enable-maintainer-mode' 'DEVELOP_WANTED=yes'"
 #define ARCHIVE_SVN  9341
