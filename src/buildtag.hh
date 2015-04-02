40f9cc8369bb86d2207d2190df86b4d94033072a
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-04-02 11:45:03 +0000

    some more buildtag rework, direntry added in libapl.texi

--- src/buildtag.hh
+++
@@ -1,3 +0,0 @@
-#include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9592:9593M", "2015-04-02 11:40:49 UTC", "Linux 3.13.0-37-generic i686", "unknown configure options"
-#define ARCHIVE_SVN " 9553"
